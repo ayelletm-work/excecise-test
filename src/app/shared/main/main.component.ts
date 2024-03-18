d5e112451e30063e25e339acea88da278b77afc1
ayelletm-work <ayelletm.work@gmail.com>
2024-03-18 14:22:18 +0200

    Update hero and main components

--- src/app/shared/main/main.component.ts
+++ src/app/shared/main/main.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   standalone: true,
+  imports: [MatCheckboxModule, FormsModule],
 
 })
 export class MainComponent {
