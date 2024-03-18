508b6700b1fd2241aeca39cd0f11a79cdbf1d1e8
ayelletm-work <ayelletm.work@gmail.com>
2024-03-18 19:44:26 +0200

    Update styles and variables

--- src/app/shared/main/main.component.ts
+++ src/app/shared/main/main.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButton, MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   standalone: true,
-  imports: [MatCheckboxModule, FormsModule],
+  imports: [MatCheckboxModule, FormsModule, MatButtonModule, MatIconModule],
 
 })
 export class MainComponent {
